feddb325e3dbe9b81c46f7ac5adcae2400be4691
Dhia Nufah <macbookprom2@Macbooks-MacBook-Pro.local>
2024-10-08 08:42:08 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// toogle navbar
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () => {
+    // remove toggle icon 
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active')
+}
+
+// typed js
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Aspiring Software Engineer', 'UI UX Designer', 'Product Manager'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+});
